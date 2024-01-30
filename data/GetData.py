36d985f48f8a20ca029ba8acc070059feabcd2a6
Omid Jaberi <omid.jaberie@gmail.com>
2024-01-30 21:19:54 +0330

    New Data added

--- data/GetData.py
+++ data/GetData.py
@@ -1,2 +1,11 @@
 import pytse_client as tse
+import pandas as pd
+
 tse.download(symbols=["فملی"], write_to_csv=True)
+
+
+#New data in old format:
+data = pd.read_csv('tickers_data/فملی.csv') 
+data.drop('yesterday', inplace=True, axis=1) 
+
+data.to_csv('tickers_data/femeli-daily.csv', encoding='utf-8', index=False)
